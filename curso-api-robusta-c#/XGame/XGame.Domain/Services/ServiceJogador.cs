828efe1192c4d3f10327b763dc29c9a7323c8c36
Anderson Olliver <pla.olliver@gmail.com>
2018-01-22 00:46:36 -0300

    Prepara Interfaces de serviço

---
+++ curso-api-robusta-c#/XGame/XGame.Domain/Services/ServiceJogador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using XGame.Domain.Arguments.Jogador;
+using XGame.Domain.Interfaces.Services;
+
+namespace XGame.Domain.Services
+{
+    public class ServiceJogador : IServiceJogador
+    {
+        public AdicionarJogadorResponse AdicionarJogador(AdicionarJogadorRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AutenticarJogadorResponse AutenticarJogador(AutenticarJogadorRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
