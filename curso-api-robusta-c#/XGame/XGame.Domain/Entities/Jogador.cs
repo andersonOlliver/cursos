04e5fb838c6e6ae572750cdfcdc8742b326a122a
Anderson Olliver <pla.olliver@gmail.com>
2018-01-22 01:40:54 -0300

    Adicionado Notification Partner

--- curso-api-robusta-c#/XGame/XGame.Domain/Entities/Jogador.cs
+++ curso-api-robusta-c#/XGame/XGame.Domain/Entities/Jogador.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using prmToolkit.NotificationPattern;
+using prmToolkit.NotificationPattern.Extensions;
+using System;
 using XGame.Domain.Enum;
+using XGame.Domain.Properties;
 using XGame.Domain.ValueObjects;
 
 namespace XGame.Domain.Entities
 {
-    public class Jogador
+    public class Jogador : Notifiable
     {
+        public Jogador(Email email, string senha)
+        {
+            Email = email;
+            Senha = senha;
+
+            new AddNotifications<Jogador>(this)
+                .IfNullOrInvalidLength(x => x.Senha, 6, 32, Message.X0_OBRIGATORIA_E_DEVE_CONTER_ENTRE_X1_E_X2_CARACTERES.ToFormat("Senha", 6, 32));
+        }
+
         public Guid Id { get; set; }
 
         public Nome Nome { get; set; }
--- curso-api-robusta-c#/XGame/XGame.Domain/Services/ServiceJogador.cs
+++ curso-api-robusta-c#/XGame/XGame.Domain/Services/ServiceJogador.cs
@@ -1,19 +1,54 @@
-﻿using System;
+﻿using prmToolkit.NotificationPattern;
+using prmToolkit.NotificationPattern.Extensions;
+using System;
 using XGame.Domain.Arguments.Jogador;
+using XGame.Domain.Entities;
+using XGame.Domain.Interfaces.Repositories;
 using XGame.Domain.Interfaces.Services;
+using XGame.Domain.Properties;
+using XGame.Domain.ValueObjects;
 
 namespace XGame.Domain.Services
 {
-    public class ServiceJogador : IServiceJogador
+    public class ServiceJogador : Notifiable, IServiceJogador
     {
+        private readonly IRepositoryJogador _repositoryJogador;
+
+        public ServiceJogador(IRepositoryJogador repositoryJogador)
+        {
+            _repositoryJogador = repositoryJogador;
+        }
+
         public AdicionarJogadorResponse AdicionarJogador(AdicionarJogadorRequest request)
         {
-            throw new NotImplementedException();
+            var email = new Email(request.Email);
+            var jogador = new Jogador(email, request.Senha);
+
+            jogador.Nome = request.Nome;
+            jogador.Status = Enum.EnumSituacaoJogador.EmAndamento;
+
+            Guid id =  _repositoryJogador.AdicionarJogador(jogador);
+
+            return new AdicionarJogadorResponse { Id = id, Message = "Operação realizada com Sucesso!" };
         }
 
         public AutenticarJogadorResponse AutenticarJogador(AutenticarJogadorRequest request)
         {
-            throw new NotImplementedException();
+            if(request == null)
+            {
+                AddNotification("AutenticarJogadorRequest", Message.X0_E_OBRIGATORIO.ToFormat("AutenticarJogadorRequest"));
+            }
+            var email = new Email(request.Email);
+            var jogador = new Jogador(email, request.Senha);
+
+            AddNotifications(jogador, email);
+
+            if (jogador.IsInvalid())
+            {
+                return null;
+            }
+            
+            return _repositoryJogador.AutenticarJogador(jogador.Email.Endereco, jogador.Senha);
         }
     }
 }
