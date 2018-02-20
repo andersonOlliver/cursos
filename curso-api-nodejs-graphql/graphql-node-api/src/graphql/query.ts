512ec7d8fec2dc7a53da6a1334b012d1d7712126
35355 <35355@CTJ-185708>
2018-02-20 18:18:07 -0400

    Adiciona schema comment

--- curso-api-nodejs-graphql/graphql-node-api/src/graphql/query.ts
+++ curso-api-nodejs-graphql/graphql-node-api/src/graphql/query.ts
@@ -1,11 +1,15 @@
 import { userQueries } from './resources/user/user.schema';
+import { postQueries } from './resources/post/post.schema';
+import { commentQueries } from './resources/comment/comment.schema';
 
 const Query = `
     type Query{
+        ${commentQueries}
         ${userQueries}
+        ${postQueries}
     }
 `;
 
-export{
+export {
     Query
 }
