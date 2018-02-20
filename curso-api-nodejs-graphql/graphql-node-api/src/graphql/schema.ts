cb658e8293e6ae640b0a741c1f98d23a7ad997c1
35355 <35355@CTJ-185708>
2018-02-20 18:00:29 -0400

    Adiciona schema

---
+++ curso-api-nodejs-graphql/graphql-node-api/src/graphql/schema.ts
@@ -0,0 +1,21 @@
+import { makeExecutableSchema } from 'graphql-tools'
+
+import { Query } from './query';
+import { Mutation } from './mutation';
+import { userTypes } from './resources/user/user.schema';
+
+const SchemaDefinition = `
+    type Schema {
+        query: Query
+        mutation: Mutation
+    }
+`;
+
+export default makeExecutableSchema({
+    typeDefs: [
+        SchemaDefinition,
+        Query,
+        Mutation,
+        userTypes
+    ]
+});
